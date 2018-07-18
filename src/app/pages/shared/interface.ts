c45d779ead4b364ced3a1fedb11abfa0d6802b7b
Umesh Mandal <umesh.mandal619@gmail.com>
2018-07-18 10:11:15 +0545

    Create interface.ts

---
+++ src/app/pages/shared/interface.ts
@@ -0,0 +1,17 @@
+
+export interface IEmployee {
+    id: number;
+    email: string;
+    imageUrl: string;
+    userName: string;
+    repeatPassword: string;
+    password: string;
+    role: string;
+    color: string[];
+    status: number;
+    country: string;
+}
+export interface ISelect {
+    id?: string;
+    name?: string;
+}
